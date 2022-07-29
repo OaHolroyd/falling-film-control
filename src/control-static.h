8d460496c06cb3eb391ed0e71e30d9a8c9f5094a
oh <oaholroyd@gmail.com>
2022-07-29 14:54:51 +0100

    static WR control

--- src/control-static.h
+++ src/control-static.h
@@ -11,6 +11,7 @@
 static double **STATIC_K; /* control operator */
 
 
+/* compute the control matrix in the Benney case */
 void benney_compute_K() {
   /* Jacobian */
   double **J = malloc_f2d(N, N);
@@ -91,6 +92,127 @@ void benney_compute_K() {
   free_2d((void **)Psi);
 }
 
+/* compute the control matrix in the weighted-residuals case */
+void wr_compute_K() {
+    /* Jacobian */
+  double **J = malloc_f2d(2*N, 2*N);
+
+  for (int i = 0; i < 2*N; i++) {
+    for (int j = 0; j < 2*N; j++) {
+      J[i][j] = 0.0;
+    } // j end
+  } // i end
+
+  double c0, c1, c2, c3, c4;
+
+  /* top right */
+  c1 = 1/(2*DX);
+  c3 = -1/(2*DX);
+  for (int i = 1; i < N-1; i++) {
+    J[i][N+i-1] = c1;
+    J[i][N+i+1] = c3;
+  } // i end
+
+  J[0][N+N-1] = c1;
+  J[0][N+1] = c3;
+
+  J[N-1][N+N-2] = c1;
+  J[N-1][N+0] = c3;
+
+  /* bottom left */
+  c0 = 1/(3*CA) * (-1/(2*DX*DX*DX));
+  c1 = (8*RE/35 - 2/tan(THETA)/3) * (-1/(2*DX)) + 1/(3*CA) * (1/(DX*DX*DX));
+  c2 = 2;
+  c3 = (8*RE/35 - 2/tan(THETA)/3) * (1/(2*DX)) + 1/(3*CA) * (-1/(DX*DX*DX));
+  c4 = 1/(3*CA) * (1/(2*DX*DX*DX));
+  for (int i = 2; i < N-2; i++) {
+    J[N+i][i-2] = c0;
+    J[N+i][i-1] = c1;
+    J[N+i][i] = c2;
+    J[N+i][i+1] = c3;
+    J[N+i][i+2] = c4;
+  } // i end
+
+  J[N+0][N-2] = c0;
+  J[N+0][N-1] = c1;
+  J[N+0][0] = c2;
+  J[N+0][1] = c3;
+  J[N+0][2] = c4;
+
+  J[N+1][N-1] = c0;
+  J[N+1][0] = c1;
+  J[N+1][1] = c2;
+  J[N+1][1+1] = c3;
+  J[N+1][1+2] = c4;
+
+  J[N+N-2][N-4] = c0;
+  J[N+N-2][N-3] = c1;
+  J[N+N-2][N-2] = c2;
+  J[N+N-2][N-1] = c3;
+  J[N+N-2][0] = c4;
+
+  J[N+N-1][N-3] = c0;
+  J[N+N-1][N-2] = c1;
+  J[N+N-1][N-1] = c2;
+  J[N+N-1][0] = c3;
+  J[N+N-1][1] = c4;
+
+  /* bottom right */
+  c1 = 6*RE/105 * 1/(2*DX);
+  c2 = -1;
+  c3 = 6*RE/105 * -1/(2*DX);
+  for (int i = 1; i < N-1; i++) {
+    J[N+i][N+i-1] = c1;
+    J[N+i][N+i] = c2;
+    J[N+i][N+i+1] = c3;
+  } // i end
+
+  J[N+0][N+N-1] = c1;
+  J[N+0][N+0] = c2;
+  J[N+0][N+1] = c3;
+
+  J[N+N-1][N+N-2] = c1;
+  J[N+N-1][N+N-1] = c2;
+  J[N+N-1][N+0] = c3;
+
+
+  /* forcing matrix (TODO: see if rotating this makes it faster) */
+  double **F = malloc_f2d(N, M);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      F[i][j] = actuator(ITOX(i)-Aloc[j]);
+    } // j end
+  } // i end
+
+
+  /* actuator matrix */
+  double **Psi = malloc_f2d(2*N, M);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      Psi[i][j] = F[i][j];
+      Psi[i+N][j] = 2*RE/15 * F[i][j];
+    } // j end
+  } // i end
+
+
+  /* full control matrix */
+  double **K = malloc_f2d(M, 2*N);
+  dlqr(J, Psi, MU, 1-MU, 2*N, M, K);
+
+  /* apply flux approximation */
+  STATIC_K = malloc_f2d(M, N);
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < N; j++) {
+      STATIC_K[i][j] = K[i][j] + 2/3 * K[i][j+N];
+    } // j end
+  } // i end
+
+
+  free_2d((void **)J);
+  free_2d((void **)F);
+  free_2d((void **)Psi);
+  free_2d((void **)K);
+}
 
 /* ========================================================================== */
 /*   FUNCTION DEFINITIONS                                                     */
@@ -103,7 +225,7 @@ void static_set() {
       benney_compute_K();
       break;
     case WR:
-      ABORT("static WR not yet implemented");
+      wr_compute_K();
       break;
     default :
       ABORT("invalid ROM type %d", RT);
