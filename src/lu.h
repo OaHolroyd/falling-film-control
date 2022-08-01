b43b79d325dd9b48893af9a51ac73bff6e2c7ef2
oh <oaholroyd@gmail.com>
2022-08-01 15:58:09 +0100

    LU factorisation and solver

---
+++ src/lu.h
@@ -0,0 +1,34 @@
+#ifndef LU_H
+#define LU_H
+
+#include <complex.h>
+
+
+/* ========================================================================== */
+/*   FUNCTION DECLARATIONS                                                    */
+/* ========================================================================== */
+/* given an n-by-n double matrix A with non-zero determinant, solves Ax = b in
+   place. */
+void dsv(double **A, double *b, int n);
+
+/* given an n-by-n double complex matrix A with non-zero determinant, solves
+   Ax = b in place. */
+void zsv(double complex **A, double complex *b, int n);
+
+/* for an n-by-n double matrix A with non-zero determinant, computes the LU
+   factorisation in place with no pivoting */
+void dlu(double **A, int n);
+
+/* for an n-by-n double complex matrix A with non-zero determinant, computes
+   the LU factorisation in place with no pivoting */
+void zlu(double complex **A, int n);
+
+/* given an n-by-n double LU factorisation LU with non-zero determinant, solves
+   LUx = b in place. */
+void dlusv(double **LU, double *b, int n);
+
+/* given an n-by-n double complex LU factorisation LU with non-zero determinant,
+   solves LUz = b in place. */
+void zlusv(double complex **LU, double complex *b, int n);
+
+#endif
