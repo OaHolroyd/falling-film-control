3c864ca7dd48a04b5e72fedc3b9d1c1f54d0d3a6
oscar <oaholroyd@gmail.com>
2025-04-12 17:11:24 +0100

    finalise setup

--- analysis.py
+++ analysis.py
@@ -1,15 +1,18 @@
-from pathlib import Path
+import numpy as np
 
 from scripts.config import Config
 from scripts.multisim import MultiSim
-from scripts.simulation import Simulation
 
 
 def main():
     config = Config.from_json("params.json")
 
+    re = np.logspace(0, 2, 20)
+    ca = np.logspace(-3, -1, 20)
+    p = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+
     multisim = MultiSim(base_dir="multirun", config=config, exe="film-ns")
-    multisim.add_variants_product(re=[1.0, 2.0], ca=[0.05, 0.01])
+    multisim.add_variants_product(re=re, ca=ca, p=p)
     multisim.run_all()
 
 
